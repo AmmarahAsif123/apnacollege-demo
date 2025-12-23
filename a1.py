0890f7fad56cd256251788289fdd6c18ac1d436e
AmmarahAsif123 <mano.pari4599@gmail.com>
2025-12-23 18:03:41 +0500

    Add files via upload

--- a1.py
+++ a1.py
@@ -1,8 +1,17 @@
-print("Sum of natural numbers")
-n=int(input("Enter the number: "))
-sum=0
-i=0
-while i<=n:
-    sum=sum+i
-    i=i+1
-print("Sum of natural numbers is: ",sum)
+# Take input of a word
+string = input("Please enter your own word : ")
+
+# Take input of a character
+char = input("Please enter your own character : ")
+
+i = 0
+count = 0
+
+# Loop to find the occurrence of the character
+while i < len(string):
+    if string[i] == char:
+        count = count + 1
+    i = i + 1  # MUST be outside the if block
+
+# Display the result
+print("The total number of times",char,"has occured =",count)
